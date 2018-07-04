b5974799d91003c1481f7d21bc5dc92aa20160c2
tk0103 <ninntenntarou@gmail.com>
2018-07-04 23:01:10 +0900

    add dice and JI
    戸澤様、確認のほど、何卒よろしくお願いいたします

--- evaluator.py
+++ evaluator.py
@@ -23,6 +23,32 @@ class UNet3DEvaluator(extensions.Evaluator):
         self.device = device
         self.eval_hook = eval_hook
 
+    def jaccard_index(self,predict, ground_truth):
+        JI_numerator=0.0
+        JI_denominator=0.0
+
+        predict = F.flatten(predict).data
+        ground_truth = F.flatten(ground_truth).data
+        seg = (predict > 0.5)
+
+        JI_numerator = (seg * ground_truth).sum()
+        JI_denominator =((seg + ground_truth)> 0).sum()
+
+        return JI_numerator/JI_denominator
+
+    def dice_coefficent(self,predict, ground_truth):
+        dice_numerator=0.0
+        dice_denominator=0.0
+
+        predict = F.flatten(predict).data
+        ground_truth = F.flatten(ground_truth).data
+        seg = (predict > 0.5)
+
+        dice_numerator = 2*(seg * ground_truth).sum()
+        dice_denominator =seg.sum()+ ground_truth.sum()
+
+        return dice_numerator/dice_denominator
+
     def evaluate(self):
         iterator = self._iterators['main']
         unet = self._targets['unet']
@@ -47,6 +73,7 @@ class UNet3DEvaluator(extensions.Evaluator):
                 eps = 1e-16
                 loss = -F.mean(F.log(predict+eps) * ground_truth)
                 observation['vali/unet/loss'] = loss
+                observation['vali/unet/dice'] = self.dice_coefficent(predict,ground_truth)
             summary.add(observation)
 
         return summary.compute_mean()
