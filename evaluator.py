fcc27661716aa9a2acb1e61ab1d15f255245aea6
tk0103 <ninntenntarou@gmail.com>
2018-07-05 19:13:32 +0900

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -23,31 +23,25 @@ class UNet3DEvaluator(extensions.Evaluator):
         self.device = device
         self.eval_hook = eval_hook
 
-    def jaccard_index(self,predict, ground_truth):
-        JI_numerator = 0.0
-        JI_denominator = 0.0
+    def loss_softmax_cross_entropy(self,predict, ground_truth):
+        eps = 1e-16
+        cross_entropy = -F.mean(F.log(predict+eps) * ground_truth)
 
-        predict = F.flatten(predict[:,1:4,:,:,:]).data
-        ground_truth = F.flatten(ground_truth[:,1:4,:,:,:]).data
-        seg = (predict > 0.5)
-
-        JI_numerator = (seg * ground_truth).sum()
-        JI_denominator =((seg + ground_truth)> 0).sum()
-
-        return JI_numerator/JI_denominator
+        return cross_entropy
 
     def dice_coefficent(self,predict, ground_truth):
         dice_numerator = 0.0
         dice_denominator = 0.0
+        eps = 1e-16
 
-        predict = F.flatten(predict[:,1:4,:,:,:]).data
-        ground_truth = F.flatten(ground_truth[:,1:4,:,:,:]).data
-        seg = (predict > 0.5)
+        predict = F.flatten(predict[:,1:4,:,:,:])
+        ground_truth = F.flatten(ground_truth[:,1:4,:,:,:].astype(np.float32))
 
-        dice_numerator = 2*(seg * ground_truth).sum()
-        dice_denominator =seg.sum()+ ground_truth.sum()
+        dice_numerator = F.sum(predict * ground_truth)
+        dice_denominator =F.sum(predict+ ground_truth)
+        dice = 2*dice_numerator/(dice_denominator+eps)
 
-        return dice_numerator/dice_denominator
+        return dice
 
     def evaluate(self):
         iterator = self._iterators['main']
@@ -70,9 +64,7 @@ class UNet3DEvaluator(extensions.Evaluator):
             with reporter_module.report_scope(observation):
                 ground_truth,data = self.converter(batch, self.device)
                 predict = unet(data)
-                eps = 1e-16
-                loss = -F.mean(F.log(predict+eps) * ground_truth)
-                observation['vali/unet/loss'] = loss
+                #observation['vali/unet/loss'] = self.dice_coefficent(predict,ground_truth)
                 observation['vali/unet/dice'] = self.dice_coefficent(predict,ground_truth)
             summary.add(observation)
 
