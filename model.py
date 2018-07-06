2fbba2a7fb2fc56f636766cfdeadda14e0156419
zEttOn <38281934+zEttOn86@users.noreply.github.com>
2018-07-06 16:05:40 +0900

    Change model

--- model.py
+++ model.py
@@ -15,46 +15,35 @@ class UNet3D(chainer.Chain):
         super(UNet3D, self).__init__()
         with self.init_scope():
             # encoder pass
-            self.ce0 = L.ConvolutionND(ndim=3, in_channels=1, out_channels=32, ksize=3, pad=1,initialW=w)
-            self.bne0 = L.BatchNormalization(32)
-            self.ce1 = L.ConvolutionND(ndim=3, in_channels=32, out_channels=64, ksize=3, pad=1,initialW=w)
-            self.bne1 = L.BatchNormalization(64)
+            self.ce0 = L.ConvolutionND(ndim=3, in_channels=1, out_channels=16, ksize=3, pad=1,initialW=w)
+            self.bne0 = L.BatchNormalization(16)
+            self.ce1 = L.ConvolutionND(ndim=3, in_channels=16, out_channels=32, ksize=3, pad=1,initialW=w)
+            self.bne1 = L.BatchNormalization(32)
 
-            self.ce2 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=64, ksize=3, pad=1, initialW=w)
-            self.bne2 = L.BatchNormalization(64)
-            self.ce3 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=128, ksize=3, pad=1, initialW=w)
-            self.bne3 = L.BatchNormalization(128)
+            self.ce2 = L.ConvolutionND(ndim=3, in_channels=32, out_channels=32, ksize=3, pad=1, initialW=w)
+            self.bne2 = L.BatchNormalization(32)
+            self.ce3 = L.ConvolutionND(ndim=3, in_channels=32, out_channels=64, ksize=3, pad=1, initialW=w)
+            self.bne3 = L.BatchNormalization(64)
 
-            self.ce4 = L.ConvolutionND(ndim=3, in_channels=128, out_channels=128, ksize=3, pad=1, initialW=w)
-            self.bne4 = L.BatchNormalization(128)
-            self.ce5 = L.ConvolutionND(ndim=3, in_channels=128, out_channels=256, ksize=3, pad=1, initialW=w)
-            self.bne5 = L.BatchNormalization(256)
-
-            self.ce6 = L.ConvolutionND(ndim=3, in_channels=256, out_channels=256, ksize=3, pad=1, initialW=w)
-            self.bne6 = L.BatchNormalization(256)
+            self.ce4 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=64, ksize=3, pad=1, initialW=w)
+            self.bne4 = L.BatchNormalization(64)
 
             # decoder pass
-            self.cd6 = L.ConvolutionND(ndim=3, in_channels=256, out_channels=512, ksize=3, pad=1, initialW=w)
-            self.bnd6 = L.BatchNormalization(512)
-            self.deconv3 = L.DeconvolutionND(ndim=3, in_channels=512, out_channels=512, ksize=2, stride=2, initialW=w, nobias=True)
+            self.cd4 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=128, ksize=3, pad=1, initialW=w)
+            self.bnd4 = L.BatchNormalization(128)
+            self.deconv2 = L.DeconvolutionND(ndim=3, in_channels=128, out_channels=128, ksize=2, stride=2, initialW=w, nobias=True)
 
-            self.cd5 = L.ConvolutionND(ndim=3, in_channels=256+512, out_channels=256, ksize=3, pad=1, initialW=w)
-            self.bnd5 = L.BatchNormalization(256)
-            self.cd4 = L.ConvolutionND(ndim=3, in_channels=256, out_channels=256, ksize=3, pad=1, initialW=w)
-            self.bnd4 = L.BatchNormalization(256)
-            self.deconv2 = L.DeconvolutionND(ndim=3, in_channels=256, out_channels=256, ksize=2, stride=2, initialW=w, nobias=True)
+            self.cd3 = L.ConvolutionND(ndim=3, in_channels=64+128, out_channels=64, ksize=3, pad=1, initialW=w)
+            self.bnd3 = L.BatchNormalization(64)
+            self.cd2 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=64, ksize=3, pad=1, initialW=w)
+            self.bnd2 = L.BatchNormalization(64)
+            self.deconv1 = L.DeconvolutionND(ndim=3, in_channels=64, out_channels=64, ksize=2, stride=2, initialW=w,nobias=True)
 
-            self.cd3 = L.ConvolutionND(ndim=3, in_channels=128+256, out_channels=128, ksize=3, pad=1, initialW=w)
-            self.bnd3 = L.BatchNormalization(128)
-            self.cd2 = L.ConvolutionND(ndim=3, in_channels=128, out_channels=128, ksize=3, pad=1, initialW=w)
-            self.bnd2 = L.BatchNormalization(128)
-            self.deconv1 = L.DeconvolutionND(ndim=3, in_channels=128, out_channels=128, ksize=2, stride=2, initialW=w,nobias=True)
-
-            self.cd1 = L.ConvolutionND(ndim=3, in_channels=64+128, out_channels=64, ksize=3, pad=1, initialW=w)
-            self.bnd1 = L.BatchNormalization(64)
-            self.cd0 = L.ConvolutionND(ndim=3, in_channels=64, out_channels=64, ksize=3, pad=1, initialW=w)
-            self.bnd0 = L.BatchNormalization(64)
-            self.lcl = L.ConvolutionND(ndim=3, in_channels=64, out_channels=num_of_label, ksize=1, pad=0, initialW=w)
+            self.cd1 = L.ConvolutionND(ndim=3, in_channels=32+64, out_channels=32, ksize=3, pad=1, initialW=w)
+            self.bnd1 = L.BatchNormalization(32)
+            self.cd0 = L.ConvolutionND(ndim=3, in_channels=32, out_channels=32, ksize=3, pad=1, initialW=w)
+            self.bnd0 = L.BatchNormalization(32)
+            self.lcl = L.ConvolutionND(ndim=3, in_channels=32, out_channels=num_of_label, ksize=1, pad=0, initialW=w)
 
     def __call__(self, x):
 
@@ -66,17 +55,10 @@ class UNet3D(chainer.Chain):
         e3 = F.relu(self.bne3(self.ce3(e2)))
         del e2
         e4 = F.relu(self.bne4(self.ce4(F.max_pooling_nd(e3, ksize=2, stride=2))))
-        e5 = F.relu(self.bne5(self.ce5(e4)))
-        del e4
-        e6 = F.relu(self.bne6(self.ce6(F.max_pooling_nd(e5, ksize=2, stride=2))))
 
         # decoder pass
-        d6 = F.relu(self.bnd6(self.cd6(e6)))
-        del e6
-        d5 = F.relu(self.bnd5(self.cd5(F.concat([self.deconv3(d6), e5]))))
-        del d6, e5
-        d4 = F.relu(self.bnd4(self.cd4(d5)))
-        del d5
+        d4 = F.relu(self.bnd4(self.cd4(e4)))
+        del e4
         d3 = F.relu(self.bnd3(self.cd3(F.concat([self.deconv2(d4), e3]))))
         del d4, e3
         d2 = F.relu(self.bnd2(self.cd2(d3)))
